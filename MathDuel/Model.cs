d2427dafaf860c32aafee0bfae5e032fbe8530fb
Rodrigo <rodrigo.pires.gamedesign@gmail.com>
2025-05-16 14:57:47 +0100

    Add Model

---
+++ MathDuel/Model.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MathDuel
+{
+    public class Model
+    {
+        private int score;
+        private int wrongAnswers;
+
+        private int a;
+        private int b;
+
+        private int operation;
+        public Model()
+        {
+            score = 0;
+            wrongAnswers = 0;
+        }
+    }
+}
